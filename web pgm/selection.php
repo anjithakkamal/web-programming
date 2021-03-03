1b092792ad0c6cd2def9dc88c0e6dd3dbfac5ca2
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2021-03-03 15:29:40 +0530

    Add files via upload

---
+++ web pgm/selection.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>id</th><th>name</th><th>branch</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$username = "20mca014";
+$password = "2387";
+$dbname = "20mca014";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, name, branch FROM student1");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+
+</body>
+</html>
